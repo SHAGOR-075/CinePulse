d2fd13c64cccabc883af375aa8dd9b105535773d
MD. KHARUL ISLAM SHAGOR <sr01885013972@gmail.com>
2025-07-27 13:09:33 +0600

    db.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/movieshuttle', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://MovieShuttle:MovieShuttle2025@movieshuttle.5ouu8uq.mongodb.net/?retryWrites=true&w=majority&appName=MovieShuttle', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
